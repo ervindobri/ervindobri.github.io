83e83527e1ceabe2fb443760374327e1659e3902
ervindobri <ervindobri@users.noreply.github.com>
2025-11-19 15:00:54 +0000

    Update portfolio from main ervindobri/portfol_io@7efd9a64971bfe5207b1493d9d175bbe3a143caa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,8 +9,8 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "icons/Icon-192.png": "8338d72fd9928ae9fd82df3af1874985",
 "icons/Icon-maskable-192.png": "8338d72fd9928ae9fd82df3af1874985",
 "manifest.json": "54c1326265f2d9d7752b84543f630c4a",
-"index.html": "04ffcee14f9874bd98df0cc2233ddcd7",
-"/": "04ffcee14f9874bd98df0cc2233ddcd7",
+"index.html": "d664ec338ab5f8e571fcff963716558a",
+"/": "d664ec338ab5f8e571fcff963716558a",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "45399331098873bfb1ab6fc1e637d32a",
@@ -98,9 +98,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "fd89cceaa15b4dfb625fe37f3d8cf098",
-"flutter_bootstrap.js": "6279e0cd4d09d3016bdd86f9c9a61f59",
+"flutter_bootstrap.js": "a7256250df360b74c7cd70a4c7fabbe0",
 "version.json": "3176d1c08bac99821c11e24786a5d2d5",
-"main.dart.js": "11ec3b8260bd81486fb5030158a0cfa5"};
+"main.dart.js": "25fb50598d092cf64a92a0e60f7c5a93"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
