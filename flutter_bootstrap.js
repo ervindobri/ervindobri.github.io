9e65ac6219f318fdb98395e56de88e4098556c01
ervindobri <ervindobri@users.noreply.github.com>
2025-11-29 18:32:49 +0000

    Update portfolio from main ervindobri/portfol_io@84962cd2adf6e427429dbae442a68c7fb9f29629

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3517490587"
+    serviceWorkerVersion: "1747406307"
   }
 });
