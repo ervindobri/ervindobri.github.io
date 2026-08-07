42a10d921e244773d526906396a6600997eb4c3b
ervindobri <ervindobri@users.noreply.github.com>
2026-08-07 18:50:41 +0000

    Update portfolio from main ervindobri/portfol_io@7a725cae9e909855c7645896077c880af52601b6

--- main.dart.js
+++ main.dart.js
@@ -39941,7 +39941,7 @@ $S:2}
 A.b1L.prototype={
 $1(a){var s=A.eH().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5a2a6a42cce67f965cf540fcecf616faca624aa1/":s)+a},
 $S:68}
 A.a3p.prototype={
 gA(a){var s=this.a
